6420c937d9b7fb029611fb9d5f171ba436110c79
rohan <rohanverma031@gmail.com>
2024-04-17 12:21:28 +0530

    hero section completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Automobiles from "@/components/Automobiles/Automobiles";
 import React from "react";
+import Hero from "./components/Hero/Hero";
 
 const page = () => {
 	return (
 		<div className="group">
+			<Hero />
 			<Automobiles />
 		</div>
 	);
