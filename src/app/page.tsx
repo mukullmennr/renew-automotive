8dc3c8d238201b29be269c6a486b135e271b5bad
Anirudh Saini <anirudh.saini852@gmail.com>
2024-04-17 14:21:05 +0530

    comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import React from "react";
 import Hero from "./components/Hero/Hero";
 
 const page = () => {
-    return (
-        <div className="group">
-            <Automobiles />
-        </div>
-    );
+    // return (
+    //     <div className="group">
+    //         <Automobiles />
+    //     </div>
+    // );
     return (
         <div className="group">
             <Hero />
