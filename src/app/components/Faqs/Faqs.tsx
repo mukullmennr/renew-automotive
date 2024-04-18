e817b3db2e4da474786e9c859f2af9ed6c99ae1a
Anirudh Saini <anirudh.saini852@gmail.com>
2024-04-18 14:00:27 +0530

    faqs

--- src/app/components/Faqs/Faqs.tsx
+++ src/app/components/Faqs/Faqs.tsx
@@ -8,26 +8,37 @@ const Faqs = () => {
         <div>
             <Container>
                 <div className={style.faq}>
-                    <div>
+                    <div className={style.accordion}>
                         <div>
                             <h3>Auto Maintenance FAQs</h3>
                         </div>
                         <div>
-                            <Accordion.Root type="single" defaultValue="item-1">
-                                <Accordion.Item value="item-1">
-                              
-                                        <Accordion.Trigger>
-                                            <h4>
-                                                How often should I have my
-                                                engine inspected and tuned up?
-                                            </h4>
-                                            <div>
-                                                <span></span>
-                                                <span></span>
-                                            </div>
-                                        </Accordion.Trigger>
-                               
-                                    <Accordion.Content>
+                            <Accordion.Root
+                                type="single"
+                                defaultValue="item-1"
+                                collapsible
+                                className={style.accordion_root}
+                            >
+                                <Accordion.Item
+                                    value="item-1"
+                                    className={style.accordion_item}
+                                >
+                                    <Accordion.Trigger
+                                        className={style.accordion_trigger}
+                                    >
+                                        <h4>
+                                            How often should I have my engine
+                                            inspected and tuned up?
+                                        </h4>
+                                        <div>
+                                            <span></span>
+                                            <span></span>
+                                        </div>
+                                    </Accordion.Trigger>
+
+                                    <Accordion.Content
+                                        className={style.accordion_content}
+                                    >
                                         <p>
                                             We recommend getting your engine
                                             inspected and tuned up every 30,000
@@ -37,16 +48,26 @@ const Faqs = () => {
                                         </p>
                                     </Accordion.Content>
                                 </Accordion.Item>
-                                <Accordion.Item value="item-1">
-                                 
-                                        <Accordion.Trigger>
-                                            <h4>
-                                                Do you offer multipoint vehicle
-                                                inspections?
-                                            </h4>
-                                        </Accordion.Trigger>
-                                 
-                                    <Accordion.Content>
+                                <Accordion.Item
+                                    value="item-2"
+                                    className={style.accordion_item}
+                                >
+                                    <Accordion.Trigger
+                                        className={style.accordion_trigger}
+                                    >
+                                        <h4>
+                                            Do you offer multipoint vehicle
+                                            inspections?
+                                        </h4>
+                                        <div>
+                                            <span></span>
+                                            <span></span>
+                                        </div>
+                                    </Accordion.Trigger>
+
+                                    <Accordion.Content
+                                        className={style.accordion_content}
+                                    >
                                         <p>
                                             We recommend getting your engine
                                             inspected and tuned up every 30,000
@@ -56,16 +77,26 @@ const Faqs = () => {
                                         </p>
                                     </Accordion.Content>
                                 </Accordion.Item>
-                                <Accordion.Item value="item-1">
-                              
-                                        <Accordion.Trigger>
-                                            <h4>
-                                                Do you offer financing options
-                                                for automotive repairs?
-                                            </h4>
-                                        </Accordion.Trigger>
-                                 
-                                    <Accordion.Content>
+                                <Accordion.Item
+                                    value="item-3"
+                                    className={style.accordion_item}
+                                >
+                                    <Accordion.Trigger
+                                        className={style.accordion_trigger}
+                                    >
+                                        <h4>
+                                            Do you offer financing options for
+                                            automotive repairs?
+                                        </h4>
+                                        <div>
+                                            <span></span>
+                                            <span></span>
+                                        </div>
+                                    </Accordion.Trigger>
+
+                                    <Accordion.Content
+                                        className={style.accordion_content}
+                                    >
                                         <p>
                                             We recommend getting your engine
                                             inspected and tuned up every 30,000
@@ -75,16 +106,26 @@ const Faqs = () => {
                                         </p>
                                     </Accordion.Content>
                                 </Accordion.Item>
-                                <Accordion.Item value="item-1">
-                                    
-                                        <Accordion.Trigger>
-                                            <h4>
-                                                Do you provide warranty coverage
-                                                for your services?
-                                            </h4>
-                                        </Accordion.Trigger>
-                                  
-                                    <Accordion.Content>
+                                <Accordion.Item
+                                    value="item-4"
+                                    className={style.accordion_item}
+                                >
+                                    <Accordion.Trigger
+                                        className={style.accordion_trigger}
+                                    >
+                                        <h4>
+                                            Do you provide warranty coverage for
+                                            your services?
+                                        </h4>
+                                        <div>
+                                            <span></span>
+                                            <span></span>
+                                        </div>
+                                    </Accordion.Trigger>
+
+                                    <Accordion.Content
+                                        className={style.accordion_content}
+                                    >
                                         <p>
                                             We recommend getting your engine
                                             inspected and tuned up every 30,000
@@ -98,7 +139,7 @@ const Faqs = () => {
                         </div>
                     </div>
                     <div className={style.appointment}>
-                        <img src="/faq.png" alt="" />
+                        {/* <img src="/faq.png" alt="" /> */}
                         <div>
                             <h3>Diagnostics, Repairs & Servicing</h3>
                             <button>Book an Appointment</button>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,16 @@ import Automobiles from "./components/Automobiles/Automobiles";
 import React from "react";
 import Hero from "./components/Hero/Hero";
 import Services from "./components/Services/Services";
-
+import Faqs from "./components/Faqs/Faqs";
 const page = () => {
-	return (
-		<div className="group">
-			<Hero />
-			<Services />
-			<Automobiles />
-		</div>
-	);
+    return (
+        <div className="group">
+            <Hero />
+            {/* <Services /> */}
+            <Faqs />
+            <Automobiles />
+        </div>
+    );
 };
 
 export default page;
